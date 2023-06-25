516c67610f1c4c814175e4226d34662b72653407
Putra Prawira <putra.prawira@idstar.co.id>
2023-06-25 19:55:24 +0700

    fix: change arrow to bottom

--- src/components/header/Banner.jsx
+++ src/components/header/Banner.jsx
@@ -10,44 +10,47 @@ function Banner() {
       <div>
         <img
           src={backgroundImage}
-          className="pt-1 w-full -z-10"
+          className="pt-1 w-full -z-10 min-h-full"
           alt="background-building"
         />
-        <div className="relative mt-[-22.5%] h-[500px] overflow-hidden">
+        <div className="relative mt-[-22.5%] md:h-[500px] overflow-hidden">
           <div className="h-full w-full absolute flex justify-center">
             <img
               src={leftImage}
               alt="image-left"
-              className="w-full absolute z-10"
+              className="w-full absolute z-10 h-[94%]"
             />
-            <div className="text-left z-30 flex items-left justify-center flex-col text-white pt-32 px-5">
-              <span className="text-4xl">Discover your wonder</span>
-              <span className="w-full text-xl pt-3">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Doloribus obcaecati temporibus itaque, reprehenderit minima
-                laborum praesentium maxime illum dignissimos perferendis?
-                Voluptates tenetur nesciunt dignissimos ab nulla esse delectus
-                architecto corrupti at beatae commodi obcaecati repudiandae rem,
-                asperiores non repellat voluptatem, minus reprehenderit
-                provident. Facere aspernatur eius inventore delectus veniam
-                mollitia architecto omnis magnam tempore officiis! Deserunt
-                rerum pariatur repudiandae quo excepturi, consectetur autem, eos
-                adipisci cumque rem veniam quibusdam maxime harum! Fuga, facere.
-                Vel est harum qui voluptate libero asperiores delectus? Magni
-                omnis ex harum! Cupiditate maiores modi ratione vero est! Illo
-                asperiores beatae, voluptas nisi mollitia veritatis dolor
-                voluptate!
-              </span>
+            <div className="flex flex-col items-left justify-between mt-[-100px] md:mt-52 px-5">
+              <div className="text-left z-30 flex items-left justify-center flex-col text-white pt-40 md:pt-0">
+                <span className="text-2xl md:text-4xl">
+                  Discover your wonder
+                </span>
+                <span className="w-full text-md md:text-xl pt-3">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Doloribus obcaecati temporibus itaque, reprehenderit minima
+                  laborum praesentium maxime illum dignissimos perferendis?
+                  Voluptates tenetur nesciunt dignissimos ab nulla esse delectus
+                  architecto corrupti at beatae commodi obcaecati
+                </span>
+              </div>
+              <div className="z-40 flex justify-center">
+                <a href="#who-we-are">
+                  <img
+                    src={scrollImage}
+                    alt="scroll-image"
+                    className="animate-bounce"
+                  />
+                </a>
+              </div>
             </div>
           </div>
-          <img
-            src={rightImage}
-            alt="image-right"
-            className="w-full z-0 relative"
-          />
-        </div>
-        <div className="flex justify-center m-[-20px] z-40 relative hover:animate-bounce">
-          <img src={scrollImage} alt="scroll-image" />
+          <div className="h-full w-full relative flex justify-center pb-6">
+            <img
+              src={rightImage}
+              alt="image-right"
+              className="w-full z-0 h-[90%]"
+            />
+          </div>
         </div>
       </div>
     </>
